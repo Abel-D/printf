c4cc591db5c266b9e10811157ed4936c6fa1bbac
Abel-D <abeldebebe818@gmail.com>
2023-01-25 18:40:00 +0300

    test commit

---
+++ printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	char *p;
+	va_list ap;
+	va_start(ap, format);
+	char *str;
+	int count = 0;
+
+	for (p = format; *p != '\0'; p++)
+	{
+		if(*p == "%")
+			p++;
+		else
+		{
+			if(*p == "c")
+			{
+
+					char x = va_arg(ap, int);
+					putchar(x);
+					putchar('\n');
+					count++;
+
+			}
+			else if(*p == "s")
+			{
+				str = va_arg(ap, char *);
+				puts(str);
+				count++;
+			}
+		}
+	}
+
+	va_end(ap);
+	return (count);
+}
+int main(void)
+{
+	_printf("%c", 'k');
+//	printf("%d\n",x);
+	return 1;
+}
